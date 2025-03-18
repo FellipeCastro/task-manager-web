6725322abc271828d6803a46a363982a340054bb
Fellipe Castro <fellipecastro27d@gmail.com>
2025-03-17 21:21:12 -0300

    Projeto atualizado

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -12,7 +12,7 @@ import "../layout/Form.css";
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
 
--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -14,9 +14,10 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
+
 
     const handleRegister = async (e) => {
         e.preventDefault();
